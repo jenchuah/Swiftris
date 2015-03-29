d9eb0479de61589c9c050316ae5cbe5398240c6d
Jen Chuah <jenmin1314@gmail.com>
2015-03-29 20:38:12 +1030

    added rules and gesture recognizers

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -9,11 +9,64 @@
 import UIKit
 import SpriteKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognizerDelegate {
 
     // Connect drawing layer and logic layer
     var scene: GameScene!
     var swiftris: Swiftris!
+    var panPointReference:CGPoint?
+    
+    @IBAction func didTap(sender: UITapGestureRecognizer) {
+        swiftris.rotateShape()
+    }
+    
+    // Every time the user's finger moves more than 90% of BlockSize points across the screen, we'll move the falling shape in the corresponding direction of the pan. We keep track of the last point on the screen at which a shape movement occured or where a pan begins.
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+        
+        // Recover a point which defines the translation of the gesture relative to where it began. This is just a measure of the distance that the user's finger has traveled.
+        let currentPoint = sender.translationInView(self.view)
+        
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                
+                // Check the velocity of the gesture to get direction. Positive velocity represents a gesture moving towards the right side of the screen, negative towards the left.
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    swiftris.moveShapeRight()
+                } else {
+                    swiftris.moveShapeLeft()
+                }
+                
+                // Reset reference point
+                panPointReference = currentPoint
+            }
+        } else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
+    }
+    
+    @IBAction func didSwipe(sender: UISwipeGestureRecognizer) {
+        swiftris.dropShape()
+    }
+    
+    // #1 Optional delegate method found in UIGestureRecognizerDelegate which will allow each gesture recognizer to work in tandem with others. However, at times a gesture recognizer may collide with another.
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        return true
+    }
+    
+    // #2 If pan gesture occur simultaneously with a swipe down gesture, to relinquish priority, thsi code performs several optional cast conditionals. These if conditionals attempt to cast the generic UIGestureRecognizer parameters as the specific types of recognizers we expect to be notified of. If the cast succeeds, the code block is executed.
+    // We let the pan gesture recognizer take precedence over the swipe gesture and the tap to do likewise over the pan. 
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldBeRequiredToFailByGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+        if let swipeRec = gestureRecognizer as? UISwipeGestureRecognizer {
+            if let panRec = otherGestureRecognizer as? UIPanGestureRecognizer {
+                return true
+            } else if let panRec = gestureRecognizer as? UIPanGestureRecognizer {
+                if let tapRec = otherGestureRecognizer as? UITapGestureRecognizer {
+                    return true
+                }
+            }
+        }
+        return false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,20 +83,21 @@ class GameViewController: UIViewController {
         scene.tick = didTick
         
         swiftris = Swiftris()
+        swiftris.delegate = self
         swiftris.beginGame()
         
         // Present the scene.
         skView.presentScene(scene)
         
         // Add nextShape to the game layer at the preview location. When that animation completes, we reposition the underlying Shape object at the starting row and starting column before we ask GameScene to move it from the preview location to its starting position. Once that completes, we ask Swiftris for a new shape, begin ticking and add the newly established upcoming piece to the preview area.
-        scene.addPreviewShapeToScene(swiftris.nextShape!) {
+        /*scene.addPreviewShapeToScene(swiftris.nextShape!) {
             self.swiftris.nextShape?.moveTo(StartingColumn, row: StartingRow)
             self.scene.movePreviewShape(self.swiftris.nextShape!) {
                 let nextShapes = self.swiftris.newShape()
                 self.scene.startTicking()
                 self.scene.addPreviewShapeToScene(nextShapes.nextShape!) {}
             }
-        }
+        }*/
     }
 
     override func prefersStatusBarHidden() -> Bool {
@@ -52,7 +106,56 @@ class GameViewController: UIViewController {
     
     // Lowers shape by one row and asks GameScene to redraw shape at new location
     func didTick() {
-        swiftris.fallingShape?.lowerShapeByOneRow()
-        scene.redrawShape(swiftris.fallingShape!, completion: {})
+        swiftris.letShapeFall()
+        /*swiftris.fallingShape?.lowerShapeByOneRow()
+        scene.redrawShape(swiftris.fallingShape!, completion: {})*/
+    }
+    
+    func nextShape() {
+        let newShapes = swiftris.newShape()
+        if let fallingShape = newShapes.fallingShape {
+            self.scene.addPreviewShapeToScene(newShapes.nextShape!){}
+            self.scene.movePreviewShape(fallingShape){
+                self.view.userInteractionEnabled = true
+                self.scene.startTicking()
+            }
+        }
+    }
+    
+    func gameDidBegin(swiftris: Swiftris) {
+        // The following is false when restarting a new game
+        if swiftris.nextShape != nil && swiftris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(swiftris.nextShape!) {
+                self.nextShape()
+            }
+        }
+        else {
+            nextShape()
+        }
+    }
+    
+    func gameDidEnd(swiftris: Swiftris) {
+        view.userInteractionEnabled = false
+        scene.stopTicking()
+    }
+    
+    func gameDidLevelUp(swiftris: Swiftris) {
+        
+    }
+    
+    func gameShapeDidDrop(swiftris: Swiftris) {
+        scene.stopTicking()
+        scene.redrawShape(swiftris.fallingShape!) {
+            swiftris.letShapeFall()
+        }
+    }
+    
+    func gameShapeDidLand(swiftris: Swiftris) {
+        scene.stopTicking()
+        nextShape()
+    }
+    
+    func gameShapeDidMove(swiftris: Swiftris) {
+        scene.redrawShape(swiftris.fallingShape!) {}
     }
 }
--- Swiftris/Swiftris.swift
+++ Swiftris/Swiftris.swift
@@ -16,6 +16,9 @@ let StartingRow = 0
 let PreviewColumn = 12
 let PreviewRow = 1
 
+let PointsPerLine = 10
+let LevelThreshold = 1000
+
 // The delegate will be notified of several events throughout the course of the game. GameViewController will implement and attach itself as delegate in order to update the user interface and react to game state changes whenever something occurs inside of swiftris.swift
 protocol SwiftrisDelegate {
     // Invoked when the current round of Swiftris ends
@@ -43,7 +46,12 @@ class Swiftris {
     var fallingShape:Shape?
     var delegate:SwiftrisDelegate?
     
+    var score:Int
+    var level:Int
+    
     init() {
+        score = 0
+        level = 1
         fallingShape = nil
         nextShape = nil
         blockArray = Array2D<Block>(columns: NumColumns, rows: NumRows)
@@ -60,6 +68,219 @@ class Swiftris {
         fallingShape = nextShape
         nextShape = Shape.random(PreviewColumn, startingRow: PreviewRow)
         fallingShape?.moveTo(StartingColumn, row: StartingRow)
+        
+        if detectIllegalPlacement() {
+            nextShape = fallingShape
+            nextShape!.moveTo(PreviewColumn, row: PreviewRow)
+            endGame()
+            return (nil, nil)
+        }
         return (fallingShape, nextShape)
     }
+
+    func detectIllegalPlacement() -> Bool {
+        if let shape = fallingShape {
+            for block in shape.blocks {
+                if block.column < 0 || block.column >= NumColumns || block.row < 0 || block.row >= NumRows {
+                    return true
+                } else if blockArray[block.column, block.row] != nil {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    // Adds the falling shape to the collection of blocks maintained by Swiftris. Once the falling shape's blocks are part of the game board, falling shape is nullified and the delegate is notified of a new shape settling into the game board
+    func settleShape() {
+        if let shape = fallingShape {
+            for block in shape.blocks {
+                blockArray[block.column, block.row] = block
+            }
+            fallingShape = nil
+            delegate?.gameShapeDidLand(self)
+        }
+    }
+    
+    // A shape should settle when one of the shapes' bottom blocks is located immediately above a block on the game board or when one of those same blocks has reached the bottom of the game board.
+    func detectTouch() -> Bool {
+        if let shape = fallingShape {
+            for bottomBlock in shape.bottomBlocks {
+                if bottomBlock.row == NumRows - 1 || blockArray[bottomBlock.column, bottomBlock.row + 1] != nil {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    func endGame() {
+        score = 0
+        level = 1
+        delegate?.gameDidEnd(self)
+    }
+    
+    func removeAllBlocks() -> Array<Array<Block>> {
+        var allBlocks = Array<Array<Block>>()
+        for row in 0..<NumRows {
+            var rowOfBlocks = Array<Block>()
+            for column in 0..<NumColumns {
+                if let block = blockArray[column, row] {
+                    rowOfBlocks.append(block)
+                    blockArray[column, row] = nil
+                }
+            }
+            allBlocks.append(rowOfBlocks)
+        }
+        return allBlocks
+    }
+    
+    //#1 Function returns tuple of two arrays - linesRemoved maintains each row of blocks which the user has filled in completely
+    func removeCompletedLines() -> (linesRemoved: Array<Array<Block>>, fallenBlocks: Array<Array<Block>>) {
+        var removedLines = Array<Array<Block>>()
+        for var row = NumRows - 1 ; row > 0; row-- {
+            var rowOfBlocks = Array<Block>()
+            
+            //#2 Iterates columns 0-9 to add every block in a given row to rowOfBlocks. If it ends up with a full set (10 blocks in total), it counts that as a removed line and adds it to the return variable.
+            for column in 0..<NumColumns {
+                if let block = blockArray[column, row] {
+                    rowOfBlocks.append(block)
+                }
+            }
+            if rowOfBlocks.count == NumColumns {
+                removedLines.append(rowOfBlocks)
+                for block in rowOfBlocks {
+                    blockArray[block.column, block.row] = nil
+                }
+            }
+        }
+        
+        //#3 If no completed lines at all, return empty arrays immediately
+        if removedLines.count == 0 {
+            return ([],[])
+        }
+        
+        //#4 Level up if points exceed their level * 1000
+        let pointsEarned = removedLines.count * PointsPerLine * level
+        score += pointsEarned
+        if score >= level * LevelThreshold {
+            level += 1
+            delegate?.gameDidLevelUp(self)
+        }
+        
+        var fallenBlocks = Array<Array<Block>>()
+        for column in 0..<NumColumns {
+            var fallenBlocksArray = Array<Block>()
+            
+            //#5 Starting in the left-most column and immediately above the bottom-most removed line, we count upwards towards the top of the game board. As we do so, we take each remaining block we find on the game board and lower it as far as possible. fallenBlocks is an array of arrays, each sub-array is filled with blocks that fell to a new position as a result of the user clearing lines beneath them.
+            for var row = removedLines[0][0].row - 1; row > 0; row-- {
+                if let block = blockArray[column, row]{
+                    var newRow = row
+                    while (newRow < NumRows - 1 && blockArray[column, newRow + 1] == nil) {
+                        newRow++
+                    }
+                    block.row = newRow
+                    blockArray[column, row] = nil
+                    blockArray[column, newRow] = block
+                    fallenBlocksArray.append(block)
+                }
+            }
+            if fallenBlocksArray.count > 0 {
+                fallenBlocks.append(fallenBlocksArray)
+            }
+        }
+        return (removedLines, fallenBlocks)
+    }
+    
+    func dropShape() {
+        if let shape = fallingShape {
+            while detectIllegalPlacement() == false {
+                shape.lowerShapeByOneRow()
+            }
+            shape.raiseShapeByOneRow()
+            delegate?.gameShapeDidDrop(self)
+        }
+    }
+    
+    // To be called once every tick
+    func letShapeFall() {
+        if let shape = fallingShape {
+            shape.lowerShapeByOneRow()
+            if detectIllegalPlacement(){
+                shape.raiseShapeByOneRow()
+                if detectIllegalPlacement() {
+                    endGame()
+                } else {
+                    settleShape()
+                }
+            } else {
+                delegate?.gameShapeDidMove(self)
+                if detectTouch() {
+                    settleShape()
+                }
+            }
+        }
+    }
+    
+    func rotateShape() {
+        if let shape = fallingShape {
+            shape.rotateClockwise()
+            if detectIllegalPlacement() {
+                shape.rotateCounterClockwise()
+            } else {
+                delegate?.gameShapeDidMove(self)
+            }
+        }
+    }
+    
+    func moveShapeLeft() {
+        if let shape =  fallingShape {
+            shape.shiftLeftByOneColumn()
+            if detectIllegalPlacement() {
+                shape.shiftRightByOneColumn()
+                return
+            }
+            delegate?.gameShapeDidMove(self)
+        }
+    }
+    
+    func moveShapeRight() {
+        if let shape = fallingShape {
+            shape.shiftRightByOneColumn()
+            if detectIllegalPlacement() {
+                shape.shiftLeftByOneColumn()
+                return
+            }
+            delegate?.gameShapeDidMove(self)
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
